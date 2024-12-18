6a354c0cc208303a0f997276df2525f919abe544
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2024-12-18 13:53:22 +0300

    Finished tasks 4-6

--- FunctionDemo/Program.cs
+++ FunctionDemo/Program.cs
@@ -23,10 +23,43 @@ int returnSpooky(int num)
         return num * num;
     }
 }
+
+double toFahrenheit(double celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
+
+void checkVowel (char c)
+{
+    if (Char.IsLetter(c))
+    {
+        // is letter
+        if (c =='a' || c =='i' || c == 'e'|| c == 'o' || c == 'u' || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U')
+        {
+            Console.WriteLine("The entered character is a vowel");
+        }
+        else
+        {
+            Console.WriteLine("The entered character is a consonant");
+        }
+    }
+    else
+    {
+        // not letter
+        Console.WriteLine("The entered character is not a letter.");
+    }
+}
+
+double currencyExchange(double amount, double rate)
+{
+    return amount * rate;
+}
 #endregion
 
 
 #region TestingTheFunctions
+
+// Task 1
 Console.WriteLine("\n");
 Console.WriteLine("Task 1:Greet Function ");
 Console.WriteLine("----------------------");
@@ -39,6 +72,7 @@ Console.WriteLine("\n");
 greet(name);
 
 
+// Task 2
 Console.WriteLine("\n");
 Console.WriteLine("Task 2:Box Volume");
 Console.WriteLine("-----------------");
@@ -59,6 +93,7 @@ Console.WriteLine("\n");
 Console.WriteLine("The Volume of the box is: "+ calculateVolume(length,width,height));
 
 
+// Task 3
 Console.WriteLine("\n");
 Console.WriteLine("Task 3:Odd or Even: ");
 Console.WriteLine("------------------- ");
@@ -67,5 +102,46 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("\n");
 Console.WriteLine("The new number produced is: "+returnSpooky(num));
+
+//Task 4
+Console.WriteLine("\n");
+Console.WriteLine("Task 4:Celsius to Fahrenheit: ");
+Console.WriteLine("----------------------------- ");
+Console.WriteLine("\n");
+
+Console.WriteLine("Please enter the temprature in celsius: ");
+Console.WriteLine("--------------------------------------- ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n");
+Console.WriteLine("The degree in Fahrenhiet is "+ toFahrenheit(celsius)+"°F");
+
+
+//Task 5
+
+Console.WriteLine("\n");
+Console.WriteLine("Task 5:Vowel or a Consonant: ");
+Console.WriteLine("\n");
+Console.WriteLine("Please enter a single character: ");
+Console.WriteLine("-------------------------------- ");
+char c = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("\n");
+checkVowel(c);
+
+//Task 6 
+Console.WriteLine("\n");
+Console.WriteLine("Task 6:Currency Exchange: ");
+Console.WriteLine("\n");
+Console.WriteLine("Please enter the amount of money you wish to convert: ");
+Console.WriteLine("----------------------------------------------------- ");
+double amount = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n");
+Console.WriteLine("Please enter the currency exchange rate: ");
+Console.WriteLine("---------------------------------------- ");
+double rate = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("The converted amount in the new currncy is "+currencyExchange(amount,rate));
 
 #endregion
